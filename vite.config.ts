928509905f7805135ab53a5fcc56624d803274ac
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2025-10-25 11:54:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import threeMinifier from "@yushijinhun/three-minifier-rollup";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -18,8 +17,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
-    threeMinifier()
+    mode === 'development' && componentTagger()
   ].filter(Boolean),
   build: {
     outDir: 'dist',
